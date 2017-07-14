5b3f8a0b94899234a1dd736d7df4510a51eb8d1d
rajkumar666 <rajkum3979@gmail.com>
2017-07-14 14:16:19 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+# hhhhh
+import java.io.*;
+import java.util.*;
+class Main
+{
+public static void main(String [] args)
+{
+iont n1;
+Scanner sc=new Scanner(System.in);
+n1=sc.nextInt();
+if(n1/2)
+{
+System.out.println(even);
+}
+else
+{
+System.out.println(odd);
+}
+}
+}
+
